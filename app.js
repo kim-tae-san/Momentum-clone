0f3c8c6697c93a2a984046ae6245c991ad6919da
kimtaesan <ekdlqld21@naver.com>
2021-07-15 16:50:56 +0900

    7/15 update

---
+++ app.js
@@ -0,0 +1,29 @@
+const loginform = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginform.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginform.classList.remove(HIDDEN_CLASSNAME);
+    loginform.addEventListener("submit", onLoginSubmit);
+}
+else {
+    paintGreetings(savedUsername);
+}
