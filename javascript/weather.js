864bbc2ced0371b94952b90c9c45533f329be95c
kimtaesan <ekdlqld21@naver.com>
2021-07-23 19:17:00 +0900

    7/23 날씨 api add

---
+++ javascript/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "d1f3b8716440dd3d85d0a60f3ab75d3e";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
